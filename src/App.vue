dec2a59d39c9268b5449bd0e620fc8bccb18559b
Riku Takano <rick.takano@gmail.com>
2020-11-21 14:58:10 +0900

    ルータの学びを深めた

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
+<div>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <router-link to="/hello">Uaaaaaaa!!!</router-link>
+  <router-link to="/bye">Oaaaaa!!!</router-link>
+  <router-view></router-view>
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld
   }
 });
 </script>
